0d8b76b60905166c0c6c46e7b92dbd15387ad81b
Kevin Herrera <1herrerake@gmail.com>
2020-12-30 15:20:12 -0600

    Successfully retreived random user data and displayed it to console. Removed gatsby boiler plate

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,14 @@ module.exports = {
   siteMetadata: {
     title: "third_and_grove_assessment",
   },
-  plugins: ["gatsby-plugin-sass"],
+  plugins: [
+    "gatsby-plugin-sass",
+    {
+      resolve: "gatsby-source-randomuser",
+      options: {
+        results: 25,
+        seed: "foobar"
+      },
+    },
+  ],
 };
