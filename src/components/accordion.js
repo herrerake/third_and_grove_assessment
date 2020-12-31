0cfff450664f08212fb8d0a1cbd10f9d68b09a9d
Kevin Herrera <1herrerake@gmail.com>
2020-12-30 21:50:52 -0600

    removed unnecessary useref

--- src/components/accordion.js
+++ src/components/accordion.js
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from "react";
+import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronDown } from "@fortawesome/free-solid-svg-icons";
 
